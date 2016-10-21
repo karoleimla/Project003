9e7f47c9ab4897cd35648267087fa4e3ef76d70e
Tehnikumi Õpilane <opilane@tptlive.ee>
2016-10-21 11:37:11 +0300

    Initial commit

---
+++ src/HelloWord.java
@@ -0,0 +1,15 @@
+/**
+ * Created by opilane on 21.10.2016.
+ */
+public class HelloWord {                      // class on methodite kogum (nimega HelloWord)
+    public static void main(String[] args) {  // method on avalduste/statements jada (nimega main)
+        System.out.println("Hello, World!");  // statement on koodi rida, mis teeb lihtsat operatsiooni
+        System.out.println("How are you?");   // another statement
+        System.out.print("Goodbye, ");
+        System.out.println("cruel world");
+        System.out.print("Hello!\nHow are you doing?\n");   // lisab uue rea (newline)
+        System.out.println("She said \"Hello!\" to me.");   // double quote
+        System.out.println("tab \ttab \ttab \ttab");   // double quote
+
+    }
+}
